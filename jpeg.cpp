1113c1a6ba695299388d2fc5d0d45a90467803f7
JohnPaulNaiju <johnpaul100574@gmail.com>
2025-08-21 21:23:49 +0530

    commit

--- jpeg.cpp
+++ jpeg.cpp
@@ -4,18 +4,10 @@
 #include <string>
 #include <cstdint>
 
-#if __BYTE_ORDER__ == __ORDER_BIG_ENDIAN__
-    const uint16_t bmpSign = 0x424D;
-#elif __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
-    const uint16_t bmpSign = 0x4D42;
-#else
-    #error "Endianness not supported"
-#endif
-
 #pragma pack(push, 1)
 
 struct fileHeader{
-    uint16_t signature{bmpSign};
+    uint16_t signature{0x4D42};
     uint32_t fileSize{0};
     uint32_t reserved{0};
     uint32_t dataOffset{0};
