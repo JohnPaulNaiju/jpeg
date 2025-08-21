f5a45977eafff4d1672faf7a7e3336eeb6e99db4
JohnPaulNaiju <johnpaul100574@gmail.com>
2025-08-21 21:11:17 +0530

    commit

--- jpeg.cpp
+++ jpeg.cpp
@@ -23,16 +23,27 @@ struct fileHeader{
 
 struct infoHeader{
     uint32_t size{0};
-    uint32_t width{0};
-    uint32_t height{0};
+    int32_t width{0};
+    int32_t height{0};
     uint16_t planes{1};
     uint16_t bitsPerPixel{0};
     uint32_t compression{0};
     uint32_t imageSize{0};
-    uint32_t xPixelsPerM{0};
-    uint32_t yPixelsPerM{0};
+    int32_t xPixelsPerM{0};
+    int32_t yPixelsPerM{0};
     uint32_t colorsUsed{0};
     uint32_t importantColors{0};
 };
 
+struct pixel{
+    uint8_t blue;
+    uint8_t green;
+    uint8_t red;
+};
+
 #pragma pack(pop)
+
+int main(){
+
+    return 0;
+};
